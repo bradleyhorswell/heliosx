089f2ce12cbd5c3d7b47df63fffba1fe220b1666
bradleyhorswell <brad0412@gmail.com>
2025-07-20 14:35:52 +0100

    fix: center text in FormSuccess component

--- src/components/FormSuccess.tsx
+++ src/components/FormSuccess.tsx
@@ -3,12 +3,12 @@ import DoneIcon from "@mui/icons-material/Done";
 
 export function FormSuccess() {
   return (
-    <Stack sx={{ gap: 1, alignItems: "center", justifyContent: "center" }}>
+    <Stack sx={{ gap: 1, alignItems: "center" }}>
       <DoneIcon color="primary" fontSize="large" />
-      <Typography variant="h5">
+      <Typography variant="h5" textAlign="center">
         Thank you for submitting your answers!
       </Typography>
-      <Typography color="textSecondary">
+      <Typography color="textSecondary" textAlign="center">
         We will be in touch shortly.
       </Typography>
     </Stack>
